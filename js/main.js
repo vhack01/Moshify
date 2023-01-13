de608dc62a8794f4bfe6cc80d784efb22b64fea8
vishwas <vishwas101kumar@gmail.com>
2023-01-14 00:20:36 +0530

    Build navigation component.

--- js/main.js
+++ js/main.js
@@ -1,8 +1,15 @@
 
 const collapse = document.querySelectorAll('.collapsible');
 collapse.forEach(val => {
-    val.addEventListener('click', function() {
-       this.classList.toggle("collapsible--expanded");
+    val.addEventListener('click', function () {
+        this.classList.toggle("collapsible--expanded");
     });
 });
 
+
+const nav_collapse = document.querySelectorAll('.nav');
+nav_collapse.forEach(val => {
+    val.addEventListener('click', function () {
+        this.classList.toggle("nav--collapsible");
+    });
+});
